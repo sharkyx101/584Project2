ac1fabbb3348973a1d94252302560ecf4a93b028
sharkyx101 <90808002+sharkyx101@users.noreply.github.com>
2023-11-29 23:05:12 -0800

    Update app.py
    fixed too

--- app.py
+++ app.py
@@ -2,23 +2,24 @@ from flask import Flask, jsonify, request
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app)  #enable CORS 
+CORS(app)  # Enable CORS for all routes
 
 @app.route('/api/file_tempo', methods=['POST', 'GET'])
 def file_tempo():
     if request.method == 'POST':
         try:
-             
+            # Assuming you have a function to process the uploaded file
             uploaded_file = request.files['my_audio_file']
             
-            #processessing file
+            # Process the file (replace this with your actual file processing logic)
+            # For demonstration purposes, let's just return the filename
             filename = uploaded_file.filename
             
             return jsonify({'status': 'success', 'filename': filename})
         except Exception as e:
             return jsonify({'status': 'error', 'message': str(e)})
     elif request.method == 'GET':
-        #return a list of sample responses
+        # For demonstration purposes, return a list of sample responses
         sample_responses = [{'filename': 'sample_file_1.mp3'}, {'filename': 'sample_file_2.mp3'}]
         return jsonify(sample_responses)
 
